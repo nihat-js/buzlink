30896e61f4fc192c1eec0dcb99d0714f14e1d900
nihat-js <abdullazadenihat@gmail.com>
2024-04-01 03:04:49 +0400

    router-link to authorization

--- client/src/components/welcome/Nav.vue
+++ client/src/components/welcome/Nav.vue
@@ -4,9 +4,10 @@
       <a href="#" class="flex items-center"><span
           class="text-indigo-600 self-center text-xl font-semibold whitespace-nowrap">BuzLink</span></a>
       <div class="flex items-center lg:order-2">
-        <a href="#"
+        <router-link to="/auth"
           class="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2">Log
-          in</a><a href="#"
+          in</router-link>
+          <a href="#"
           class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2">Get
           Started</a><button type="button"
           class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
