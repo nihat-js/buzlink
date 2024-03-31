7029f03d827033b0956a88fd45b43f8b844d1548
nihat-js <abdullazadenihat@gmail.com>
2024-03-31 20:14:46 +0400

    client added

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -8,6 +8,17 @@ module.exports = {
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
+  overrides: [
+    {
+      files: [
+        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
+        'cypress/support/**/*.{js,ts,jsx,tsx}'
+      ],
+      'extends': [
+        'plugin:cypress/recommended'
+      ]
+    }
+  ],
   parserOptions: {
     ecmaVersion: 'latest'
   }
